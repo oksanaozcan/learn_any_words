58b355dc9c2915253312f768c4d09718741765ea
oksanaozcan <mekansa83@gmail.com>
2021-11-20 16:48:34 +0300

    npm i expo-file-system

--- src/store/actions/wordAction.js
+++ src/store/actions/wordAction.js
@@ -1,10 +1,15 @@
+import * as FileSystem from 'expo-file-system';
+import DB from "../../db";
 import { LOAD_WORDS, TOGGLE_FAVORITE, TOGGLE_LEARNED, REMOVE_WORD, ADD_WORD } from "../types";
 
-export const loadWords = () => {
-  return {
-    type: LOAD_WORDS,
-    payload: []
-  }
+export const loadWords = () => { 
+  return async dispatch => {
+    const words = await DB.getWords()
+    dispatch({
+      type: LOAD_WORDS,
+      payload: words
+    })
+  }  
 }
 
 export const toggleFavorite = id => {
@@ -28,10 +33,10 @@ export const removeWord = id => {
   }
 }
 
-export const addWord = newWord => {
-  newWord.id = Date.now().toString()
-  return {
+export const addWord = newWord => async dispatch => {
+  
+  dispatch({
     type: ADD_WORD,
     payload: newWord
-  }
+  })
 }
