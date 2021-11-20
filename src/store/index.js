158108f28f0a1bf4058a787169a66afb3fad1b4a
oksanaozcan <mekansa83@gmail.com>
2021-11-20 16:37:01 +0300

    npm i redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import wordReducer from './reducers/wordReducer';
 
 const rootReducer = combineReducers({
-  word: wordReducer
+  word: wordReducer, 
 })
 
-export default createStore(rootReducer)
+export default createStore(rootReducer, applyMiddleware(thunk))
