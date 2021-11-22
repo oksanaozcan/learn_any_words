f0f37dd60d6d74bf8322318138c6f83a3538349e
oksanaozcan <mekansa83@gmail.com>
2021-11-22 17:35:04 +0300

    btns added

--- src/screens/AllWordsTreningScreen.js
+++ src/screens/AllWordsTreningScreen.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { useSelector } from "react-redux";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Button, Text } from "react-native";
 import { THEME } from "../theme";
 import Card from "../components/Card";
 import TitleText from "../components/TitleText";
@@ -28,45 +28,71 @@ const AllWordsTreningScreen = ({}) => {
     )
   } else {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Card>
-            <TitleText titleStyle={styles.titleStyle}>{word.word}</TitleText>
-          </Card>
-          <Card thiscard={styles.thiscard}>            
-            <SubText >translate:</SubText>          
-            <CommonText>{word.translate}</CommonText>             
-          </Card>        
-          <Card>
-            <SubText>synonims:</SubText>          
-            <CommonText>{word.synonims}</CommonText>       
-            <SubText >category:</SubText>          
-            <CommonText>{word.category}</CommonText>             
-          </Card>      
-          <Card>
-            <SubText>example:</SubText>          
-            <CommonText>{word.example}</CommonText>       
-            <SubText >translate example:</SubText>          
-            <CommonText>{word.tr_example}</CommonText>             
-          </Card>
-        </View> 
-      </ScrollView>
+      <View style={styles.main}>     
+        <ScrollView>
+          <View style={styles.container}>
+            <Card>
+              <TitleText titleStyle={styles.titleStyle}>{word.word}</TitleText>
+            </Card>
+            <Card thiscard={styles.thiscard}>            
+              <SubText >translate:</SubText>          
+              <CommonText>{word.translate}</CommonText>             
+            </Card>        
+            <Card>
+              <SubText>synonims:</SubText>          
+              <CommonText>{word.synonims}</CommonText>       
+              <SubText >category:</SubText>          
+              <CommonText>{word.category.repeat(200)}</CommonText>             
+            </Card>      
+            <Card>
+              <SubText>example:</SubText>          
+              <CommonText>{word.example}</CommonText>       
+              <SubText >translate example:</SubText>          
+              <CommonText>{word.tr_example}</CommonText>             
+            </Card>
+          </View>         
+        </ScrollView>
+        <View style={styles.footerBtnContainer}>
+          <View style={styles.btn}>
+            <Button title="I know this word, scip it" onPress={() => console.log('bottombtn1')}/>
+          </View>
+          <View style={styles.btn}>
+            <Button title="bottom_2" onPress={() => console.log('bottombtn1')}/>
+          </View>         
+        </View>
+      </View>
+      
+      
     )
   }
 }
 
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center'
+  },
   container: {
-    padding: 20,
+    padding: 15,
     alignItems: 'center',
     width: '95%'
   },
   titleStyle: {
     textAlign: 'center',
-    padding: 25,
+    padding: 15,
   },    
-  
+  footerBtnContainer: {
+    paddingVertical: 25,
+    flexDirection: 'row',
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  btn: {
+    width: '48%'
+  },
+ 
 })
 
 export default AllWordsTreningScreen;
