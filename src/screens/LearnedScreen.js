6b33138f1d87fc6eca65ab2960a31aa290d2f849
oksanaozcan <mekansa83@gmail.com>
2021-11-23 10:40:07 +0300

    headerbtn for LearnedScreen

--- src/screens/LearnedScreen.js
+++ src/screens/LearnedScreen.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { StyleSheet, SafeAreaView, FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import React, {useLayoutEffect, useEffect} from "react";
+import { StyleSheet, SafeAreaView, FlatList, View, Alert } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { Ionicons } from '@expo/vector-icons';
+import { THEME } from "../theme";
 import WordItem from "../components/WordItem";
+import { toggleLearned, removeWord, loadWords } from "../store/actions/wordAction";
 
 const LearnedScreen = ({navigation}) => {  
-
+  const dispatch = useDispatch()
   const renderItem = ({ item }) => (
     <WordItem item={item} 
       openWord={() => navigation.navigate('Word', 
@@ -15,7 +18,58 @@ const LearnedScreen = ({navigation}) => {
     />
   );
 
-  const learnedWords = useSelector(state => state.word.learnedWords)
+  const learnedWords = useSelector(state => state.word.learnedWords) 
+
+  useEffect(() => {
+    dispatch(loadWords())
+  }, [learnedWords, dispatch])  
+
+  const toggleAllLearnedWords = () => 
+    Alert.alert(
+      `Unmark learned from all words`,
+      "Words will remain available in other categories",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "Clear", onPress: () => {
+          learnedWords.map(obj => {
+            dispatch(toggleLearned(obj))
+          })                    
+        }}          
+      ]
+    );
+
+  const removeAllLearnedWords = () => 
+  Alert.alert(
+    `Remove learned words`,
+    "Do you really want to remove all learned words? This action cannot be undone!",
+    [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      },
+      { text: "Delete", onPress: () => {
+        learnedWords.map(obj => {
+          dispatch(removeWord(obj.id))
+        })                      
+      }}          
+    ]
+  );
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View style={styles.headericonContainer}>          
+          <Ionicons name="refresh-outline" size={25} color={THEME.GREEN_COLOR} onPress={toggleAllLearnedWords}/>
+          <Ionicons name="trash-outline" size={25} color={THEME.PINK_COLOR} onPress={removeAllLearnedWords}/>
+        </View>            
+      ),   
+    });
+  }, [navigation]);
 
   return(
     <SafeAreaView>
@@ -29,7 +83,11 @@ const LearnedScreen = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
-  
+  headericonContainer: {
+    flexDirection: 'row',
+    width: 65,
+    justifyContent: 'space-between'
+  }  
 })
 
 export default LearnedScreen;
