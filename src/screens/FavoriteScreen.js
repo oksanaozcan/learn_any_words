306e38bc99a7273199215710e998f39741d5ca93
oksanaozcan <mekansa83@gmail.com>
2021-11-23 12:01:20 +0300

    header btn for favoriteScreen & functions

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { StyleSheet, SafeAreaView, FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import React, {useLayoutEffect, useEffect} from "react";
+import { StyleSheet, SafeAreaView, FlatList, View, Alert } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import WordItem from "../components/WordItem";
+import { Ionicons } from '@expo/vector-icons';
+import { THEME } from "../theme";
+import { toggleFavorite, removeWord, loadWords } from "../store/actions/wordAction";
 
 const FavoriteScreen = ({navigation}) => {
-  
+  const dispatch = useDispatch()  
   const renderItem = ({ item }) => (
     <WordItem item={item} 
       openWord={() => navigation.navigate('Word', 
@@ -16,6 +19,56 @@ const FavoriteScreen = ({navigation}) => {
   );
 
   const favoriteWords = useSelector(state => state.word.favoriteWords)
+  useEffect(() => {
+    dispatch(loadWords())
+  }, [favoriteWords, dispatch])  
+
+  const toggleAllFavoriteWords = () => 
+    Alert.alert(
+      `Unmark favorite from all words`,
+      "Words will remain available in other categories",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "Clear", onPress: () => {
+          favoriteWords.map(obj => {
+            dispatch(toggleFavorite(obj))
+          })                    
+        }}          
+      ]
+    );
+
+  const removeAllFavoriteWords = () => 
+  Alert.alert(
+    `Remove favorite words`,
+    "Do you really want to remove all favorite words? This action cannot be undone!",
+    [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      },
+      { text: "Delete", onPress: () => {
+        favoriteWords.map(obj => {
+          dispatch(removeWord(obj.id))
+        })                      
+      }}          
+    ]
+  );
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View style={styles.headericonContainer}>          
+          <Ionicons name="heart-dislike" size={25} color={THEME.PINK_COLOR} onPress={toggleAllFavoriteWords}/>
+          <Ionicons name="trash-outline" size={25} color={THEME.PINK_COLOR} onPress={removeAllFavoriteWords}/>
+        </View>            
+      ),   
+    });
+  }, [navigation]);
 
   return(
     <SafeAreaView>
@@ -29,7 +82,11 @@ const FavoriteScreen = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
-  
+  headericonContainer: {
+    flexDirection: 'row',
+    width: 65,
+    justifyContent: 'space-between'
+  }    
 })
 
 export default FavoriteScreen;
