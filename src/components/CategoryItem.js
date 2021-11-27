09577da5cb36c2adc0812c4aa1cc30ffd7f02275
oksanaozcan <mekansa83@gmail.com>
2021-11-27 11:26:06 +0300

    fixed styles bug CategoryItem

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { StyleSheet, View } from 'react-native';
 import { ListItem } from 'react-native-elements'
+import { THEME } from "../theme";
 
 const CategoryItem = ({ item, openCategory }) => {
   const catWords = useSelector(state => state.word.allWords.filter(w => w.category === item))
   const learnedWords = catWords.filter(w => w.learned).length
   return (   
     <View>
-    <ListItem style={styles.container} bottomDivider onPress={openCategory}>   
+    <ListItem style={styles.container} bottomDivider onPress={openCategory} containerStyle={{ backgroundColor: 'transparent' }}>   
       <ListItem.Title>{item ? item : 'not name'}</ListItem.Title>  
       <ListItem.Content>        
         <ListItem.Subtitle>[{learnedWords ? learnedWords : 'no learned'} / {catWords.length}]</ListItem.Subtitle>
@@ -22,12 +23,13 @@ const CategoryItem = ({ item, openCategory }) => {
 
 const styles = StyleSheet.create({ 
   container: {
-    position: 'relative'
+    position: 'relative'    
   },
   progressLine: {
     position: 'absolute',    
     height: '100%',
-    backgroundColor: 'rgba(77,182,172,0.28)'
+    backgroundColor: THEME.GREEN_COLOR,    
+    zIndex: -1
   }
 });
 
